b3efcf527346d1eadc9fd091a31a2422ea840b4b
Tihran Katolikian <moreggmoreskill@icloud.com>
2019-01-19 15:38:14 +0200

    Setup project

--- SleepEngine/stdafx.h
+++ SleepEngine/stdafx.h
@@ -1,3 +1,20 @@
 #pragma once
 
+#pragma region Standard headers
+#include <cassert>
 #include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <memory>
+#include <filesystem>
+#include <string>
+#include <string_view>
+#include <functional>
+#include <chrono>
+#include <cmath>
+#include <thread>
+#include <mutex>
+#pragma endregion
