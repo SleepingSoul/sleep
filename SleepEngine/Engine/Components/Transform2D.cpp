4e904e56a126c672ab1cb4a7f18669219fac9cb7
Tihran Katolikian <moreggmoreskill@icloud.com>
2019-03-31 00:05:56 +0200

    Add getters/setters to Transform2DData

--- SleepEngine/Engine/Components/Transform2D.cpp
+++ SleepEngine/Engine/Components/Transform2D.cpp
@@ -6,13 +6,6 @@ BEGIN_NAMESPACE_SLEEP
 
 Transform2D::Transform2D() noexcept(true)
     : Base(getComponentTypeID <Transform2D>())
-    , m_data{
-        { 1.f, 1.f },
-        { 0.f, 0.f },
-        { 1.f, 1.f },
-        1,
-        0.f
-      }
 {}
 
 END_NAMESPACE_SLEEP
--- SleepEngine/Engine/Transform2DData.h
+++ SleepEngine/Engine/Transform2DData.h
@@ -6,11 +6,25 @@ BEGIN_NAMESPACE_SLEEP
 
 struct Transform2DData
 {
-    glm::vec2 scale;
-    glm::vec2 position;
-    glm::vec2 size;
-    Layer layer;
-    float rotation;
+public:
+    Transform2DData(glm::vec2 scale = {1.f, 1.f},
+        glm::vec2 position = {0.f, 0.f},
+        glm::vec2 size = {1.f, 1.f},
+        Layer layer = 1,
+        float rotation = 0.f) noexcept(true);
+
+    GET_SET(glm::vec2, getScale, setScale, m_scale)
+    GET_SET(glm::vec2, getPosition, setPosition, m_position)
+    GET_SET(glm::vec2, getSize, setSize, m_size)
+    GET_SET(Layer, getLayer, setLayer, m_layer)
+    GET_SET(float, getRotation, setRotation, m_rotation)
+
+private:
+    glm::vec2 m_scale;
+    glm::vec2 m_position;
+    glm::vec2 m_size;
+    Layer m_layer;
+    float m_rotation;
 };
 
 END_NAMESPACE_SLEEP
