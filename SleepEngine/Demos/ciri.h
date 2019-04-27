6d78f3f7a970f9ff923c29171faffd0695b2ce29
TarasMartynyuk <tarasmartynyuk22@gmail.com>
2019-04-27 17:50:16 +0300

    sep ciri

---
+++ SleepEngine/Demos/ciri.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void initCiriScene(slp::Game::Scene& scene);
--- SleepEngine/main.cpp
+++ SleepEngine/main.cpp
@@ -20,39 +20,6 @@ int main()
     game.getClock().setFPS(60.f);
     game.getClock().setRestrictFPS(true);
 
-    auto initMainScene = [](slp::Game::Scene& scene)
-    {
-        auto& game = slp::Game::instance();
-
-        auto background = slp::createRenderableObject();
-
-        auto* const renderer = background->getComponent <slp::Renderer>();
-        auto* const transform2D = background->getComponent <slp::Transform2D>();
-
-        renderer->setTexture(game.getResourceManager().getTexture(slp::Textures::Orphea));
-        transform2D->setSize(slp::pixelsToMeters(game.getCamera().getScreenSize()));
-        transform2D->setLayer(0);
-
-        scene.addToRoot(std::move(background));
-
-        auto ciriTexture = game.getResourceManager().getTexture(slp::Textures::Cirilla);
-
-        for (int i = 0; i < 200; i++)
-        {
-            auto ciri = slp::createRenderableObject();
-
-            auto* const renderer = ciri->getComponent <slp::Renderer>();
-            auto* const transform2D = ciri->getComponent <slp::Transform2D>();
-
-            renderer->setTexture(ciriTexture);
-            transform2D->setLayer(1);
-            transform2D->setRotation(static_cast <float>(i) * 4.4f);
-            transform2D->setSize(slp::pixelsToMeters(ciriTexture->getSize()));
-            transform2D->setPosition({ -slp::pixelsToMeters(game.getCamera().getScreenWidth() / 2.f) + i++ / 20.f, 0.f });
-            scene.addToRoot(std::move(ciri));
-        }
-    };
-
     game.getResourceManager().preload();
     game.addScene(initBubbleDemoScene);
 	game.run();
