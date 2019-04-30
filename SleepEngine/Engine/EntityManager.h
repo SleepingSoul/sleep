9d9660d1ae432110d914126899364b896fefd4c5
Tihran Katolikian <moreggmoreskill@icloud.com>
2019-05-01 00:30:22 +0300

    Entity manager concept

---
+++ SleepEngine/Engine/EntityManager.h
@@ -0,0 +1,20 @@
+#pragma once
+
+BEGIN_NAMESPACE_SLEEP
+
+class EntityManager
+{
+    FORBID_COPY_AND_MOVE(EntityManager)
+public:
+    using EntitiesContainer = std::vector<std::unique_ptr<Object>>;
+
+    void addObject(std::unique_ptr <Object>&& object);
+    void removeObject(Object const* object);
+
+    void update(float dt);
+
+private:
+    EntitiesContainer m_objects;
+};
+
+END_NAMESPACE_SLEEP
