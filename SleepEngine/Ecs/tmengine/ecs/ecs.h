55ac39e80758b2d5f5f8528ecdb745e064adb1bd
Taras Martyniuk <TarasMartynyuk22@gmail.com>
2019-09-02 18:30:45 +0300

    - ecs

--- SleepEngine/Ecs/tmengine/ecs/ecs.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by Taras Martyniuk on 7/29/2018.
-//
-#ifndef SFML_GAME_ENGINE_ECS_H
-#define SFML_GAME_ENGINE_ECS_H
-
-#include <ecs/signatures/signatures.h>
-
-#endif //SFML_GAME_ENGINE_ECS_H
--- SleepEngine/Ecs/tmengine/main.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Taras on 7/24/2018.
-//
-
-
-int main()
-{
-
-}
