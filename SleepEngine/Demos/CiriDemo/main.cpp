c8a46b4dd796023372e650e325e390447cb5743a
Taras Martyniuk <TarasMartynyuk22@gmail.com>
2019-09-02 18:43:48 +0300

    engien as exe works

--- SleepEngine/Demos/CiriDemo/main.cpp
+++ SleepEngine/Demos/CiriDemo/main.cpp
@@ -1,10 +1,10 @@
 #include "stdafx.h"
-//#include <Engine/ResourceManagement/ResourceManager.h>
-//#include <Engine/Utils/unit_utils.h>
-//#include <Engine/object_shortcuts.h>
-//#include <Engine/Jobs/JobSystem.h>
-//#include <Engine/Render/UpdateRenderBridge.h>
-//#include <Engine/GameTemplate/Game.h>
+#include <Engine/ResourceManagement/ResourceManager.h>
+#include <Engine/Utils/unit_utils.h>
+#include <Engine/object_shortcuts.h>
+#include <Engine/Jobs/JobSystem.h>
+#include <Engine/Render/UpdateRenderBridge.h>
+#include <Engine/GameTemplate/Game.h>
 //#include "ciri.h"
 
 // settings
@@ -17,21 +17,21 @@ namespace
 
 int main()
 {
-    //EASY_PROFILER_ENABLE;
+    EASY_PROFILER_ENABLE;
 
-    ////slp::Object o;
-    ////o.update(2.f);
+    slp::Object o;
+    o.update(2.f);
 
-    //slp::Game game(WindowWidth, WindowHeight);
+    slp::Game game(WindowWidth, WindowHeight);
 
-    //game.getClock().setFPS(60.f);
-    //game.getClock().setRestrictFPS(true);
+    game.getClock().setFPS(60.f);
+    game.getClock().setRestrictFPS(true);
 
-    //game.getResourceManager().preload();
+    game.getResourceManager().preload();
     //game.addScene(initCiriScene, "siri");
     //game.setScene("siri");
-    //game.run();
+    game.run();
 
-    ////profiler::dumpBlocksToFile("../profiles/last_session_profile.prof");
-    //return 0;
+    //profiler::dumpBlocksToFile("../profiles/last_session_profile.prof");
+    return 0;
 }
